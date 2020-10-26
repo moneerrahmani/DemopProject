4ec88cfe968d480ce039295caa397942d361ae81
Rahmani Moneer <rki6476@hca.corpad.net>
2020-10-26 19:50:53 -0400

    This is Demo Project

---
+++ src/main/java/Exercise/demo/maven/project/App.java
@@ -0,0 +1,13 @@
+package Exercise.demo.maven.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
